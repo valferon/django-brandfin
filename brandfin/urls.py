865087af9b3e0bcea88c01fbfa7e29406290224c
Valentin Feron <vferon@brandscreen.com>
2015-05-29 11:03:33 +1000

    fix removed import

--- brandfin/urls.py
+++ brandfin/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, url
 
-from views import QueryView, CreateQueryView, PlayQueryView, DeleteQueryView, ListQueryView, ListQueryLogView, RefreshQueryView, TemplateView, CreateTemplateView, DeleteTemplateView, JqueryView
-
+from views import QueryView, CreateQueryView, PlayQueryView, DeleteQueryView, ListQueryView, ListQueryLogView, RefreshQueryView, TemplateView, CreateTemplateView, DeleteTemplateView
 
 urlpatterns = patterns('',
                        url(r'template_(?P<template_id>\d+)/$', TemplateView.as_view(), name='template_detail'),
@@ -18,6 +17,5 @@ urlpatterns = patterns('',
                        url(r'schema/$', 'brandfin.views.schema', name='explorer_schema'),
                        url(r'logs/$', ListQueryLogView.as_view(), name='explorer_logs'),
                        url(r'format/$', 'brandfin.views.format_sql', name='format_sql'),
-                       url(r'jquery/$', JqueryView.as_view(), name='jquery'),
                        url(r'^$', ListQueryView.as_view(), name='explorer_index'),
                        )
